4ba0b949142f10fb09bfde4f1a16c6107a75a411
clampju <clampju@126.com>
2023-05-06 12:41:15 +0800

    添加钉钉机器人接入功能2

--- src/main/java/com/openai/controller/OpenAiProjectController.java
+++ src/main/java/com/openai/controller/OpenAiProjectController.java
@@ -23,6 +23,6 @@ public class OpenAiProjectController {
     @PostMapping("/chatCompletion")
     @ResponseBody
     public List<String> chatCompletion(@RequestBody ChatCompletion chatCompletion) {
-        return openAiProjectService.getOpenaiMessageList(chatCompletion.getPrompt(),chatCompletion.getUserId());
+        return openAiProjectService.getOpenaiMessageList(chatCompletion.getUserId(),chatCompletion.getPrompt());
     }
 }
