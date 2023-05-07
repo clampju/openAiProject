5549deb9a2a1b835fe78419510a13818e48c410a
clampju <clampju@126.com>
2023-05-07 22:59:11 +0800

    更新OpenAiProjectController

--- src/main/java/com/openai/controller/OpenAiProjectController.java
+++ src/main/java/com/openai/controller/OpenAiProjectController.java
@@ -3,6 +3,9 @@ package com.openai.controller;
 import com.openai.config.OpenAiProjectConfig;
 import com.openai.model.ChatCompletion;
 import com.openai.service.OpenAiProjectService;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionResult;
+import com.theokanning.openai.service.OpenAiService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -12,17 +15,18 @@ import java.util.List;
  * chat gpt控制器
  */
 @RestController
-@RequestMapping("/openai")
+@RequestMapping("/v1/chat")
 public class OpenAiProjectController {
     @Resource
     private OpenAiProjectService openAiProjectService;
-
+    private static OpenAiService openAiService;
     /**
      * 简单聊天接口
      */
-    @PostMapping("/chatCompletion")
+    @PostMapping("/completions")
     @ResponseBody
-    public List<String> chatCompletion(@RequestBody ChatCompletion chatCompletion) {
-        return openAiProjectService.getOpenaiCompletionMsgList(chatCompletion.getUserId(),chatCompletion.getPrompt());
+    public ChatCompletionResult chatCompletion(@RequestBody ChatCompletionRequest chatCompletionRequest) {
+        return openAiService.createChatCompletion(chatCompletionRequest);
+        //return openAiProjectService.getOpenaiCompletionMsgList(chatCompletion.getUserId(),chatCompletion.getPrompt());
     }
 }
